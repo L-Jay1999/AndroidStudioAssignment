0a0b17161883f5a849919799bd02dee500f49423
liujiayu05 <liujiayu05@kuaishou.com>
2020-04-20 22:39:30 +0800

    fifth commit

---
+++ Chapter-5/Homework/app/src/main/java/com/byted/chapter5/User.java
@@ -0,0 +1,24 @@
+package com.byted.chapter5;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * "admin": false,
+ * "chapterTops": [],
+ * "collectIds": [],
+ * "email": "",
+ * "icon": "",
+ * "id": 58872,
+ * "nickname": "wwwsjdajsd",
+ * "password": "",
+ * "publicName": "wwwsjdajsd",
+ * "token": "",
+ * "type": 0,
+ * "username": "wwwsjdajsd"
+ */
+class User {
+    @SerializedName("nickname")
+    public String nickname;
+    @SerializedName("id")
+    public int id;
+}
